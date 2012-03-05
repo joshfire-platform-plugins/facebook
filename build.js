214c481dd48363ba6df323f4ae16a9c130413ecc
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2012-03-05 17:54:19 +0100

    i'm a d**

--- build.js
+++ build.js
@@ -171,11 +171,11 @@ module.exports = {
     //todo!
     if (params["deployer"]!="xcodeproj") {
 
-      add+="(function() {"+
+      add+="<"+"script>(function() {"+
           "var e = document.createElement('script'); e.async = true;"+
           "e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';"+
           "document.getElementById('fb-root').appendChild(e);"+
-          "}());";
+          "}());</"+"script>";
       
     }
 
